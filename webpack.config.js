ccc2123912e9883e14ab1ea610ff7d793c0dcb61
DenKorablev <denis18051990@mail.ru>
2022-01-14 18:39:25 +0300

    Выполнил задание по подключению webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 4200,
+  }
+}
